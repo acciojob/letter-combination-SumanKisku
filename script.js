f4a6dd3c5517104cdfb32f8f203b802ecba26b72
Suman Kisku <sumankisku1@gmail.com>
2023-01-16 12:36:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	const keys = {
+		1: "1",
+		2: "abc",
+		3: "def",
+		4: "ghi",
+		5: "jkl",
+		6: "mno",
+		7: "pqrs",
+		8: "tuv",
+		9: "wxyz",
+		0: "0"
+	}
+	for(key in keys){
+		console.log(keys[key]);
+	}
 }
 
 module.exports = letterCombinations;
